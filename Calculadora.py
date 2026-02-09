798c37ece870dc3bcea666b8e1178ac26275fd2b
Kauan Stipp Pedroso <kauan.stipp.pedroso@escola.pr.gov.br>
2026-02-09 14:11:25 +0000

    Aula1

---
+++ Calculadora.py
@@ -0,0 +1,9 @@
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+soma = num1 + num2
+sub = num1 - num2
+div = num1 / num2
+mult = num1 * num2
+
+print("O resultado do número é soma =", soma, )
---
+++ ola.py
@@ -0,0 +1,7 @@
+# nome = input("Digite seu nome: ")
+# print("Olá,",nome , "Sejá bem vindo")
+
+#nome = input("Digite seu nome: ")
+#sobrenome = input("Digite seu sobrenome: ")
+#local = input("Digite seu endereço: ")
+# print("Seu nome é", nome, sobrenome, "Sua casa fica na rua", local)
