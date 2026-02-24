f56c12f352a48cbf0fe03a045860cacd716e1bcd
Kauan Stipp Pedroso <kauan.stipp.pedroso@escola.pr.gov.br>
2026-02-24 12:31:45 +0000

    Dificuldade

--- Adivinhacao.py
+++ Adivinhacao.py
@@ -1,17 +1,20 @@
+import random
 print('*************************')
-print('***Jogo da adivinhação***')
+print('** Jogo da adivinhação **')
 print('*************************')
 
-numero_secreto = 67
+numero_secreto = random.randrange( 1,101 )
 total_tentativas = 3
-rodada = 1
 
-while ( rodada <= total_tentativas ):
-    chute_str = input( "Digite o seu número: " )
-
+for rodada in range(1, total_tentativas +1 ):
+    print("Tentativa {} de {}".format(rodada,total_tentativas))
+    
+    chute_str = input( "Digite o seu número:")
     print( "Seu número é", chute_str )
-
     chute = int(chute_str)
+
+    if(chute <1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
 
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
@@ -22,9 +25,8 @@ while ( rodada <= total_tentativas ):
         break
     else:
         if( maior ):
-            print("O seu chute foi maior que o número secreto")
+            print("O seu chute foi maior que o número secreto tente no próximo o número um pouco menor")
         elif( menor ):
-            print("O seu chute foi menor que o número secreto")
-    rodada = rodada + 1
+            print("O seu chute foi menor que o número secreto tente no próximo o número um pouco maior")
 
 print("Game over, o número era", numero_secreto)
